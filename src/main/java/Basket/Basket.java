334671c7d4229ded6251b2e7617ccb7e668e34f9
oskarwilms1 <o.wilms.062@studms.ug.edu.pl>
2024-11-24 19:58:42 +0100

    add improved sorting mechanisms??? i think???

--- src/main/java/Basket/Basket.java
+++ src/main/java/Basket/Basket.java
@@ -1,23 +1,30 @@
-package Basket ;
+package Basket;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
 public class Basket {
     private Product[] listOfProducts = new Product[0];
-
+    private String HowtoSort = "getPrice";
     public void addProduct(Product prod) {
+        if (prod == null) {
+            throw new IllegalArgumentException("Product cannot be null");
+        }
         this.listOfProducts = increaseBasketSize(prod);
-        Arrays.sort(listOfProducts, new PriceAndNameComparator());
+        Arrays.sort(listOfProducts, new PriceAndNameComparator(HowtoSort)); // Default sorting by price
     }
 
     public Product[] increaseBasketSize(Product prod) {
-        Product[] newBasket  = new Product[listOfProducts.length + 1];
+        Product[] newBasket = new Product[listOfProducts.length + 1];
         System.arraycopy(listOfProducts, 0, newBasket, 0, listOfProducts.length);
         newBasket[listOfProducts.length] = prod;
         return newBasket; 
     }
+    public void ChangeSortSettings(String methodName){
+        this.HowtoSort = methodName;
+    }
 
-    public Product[] getBasket () {
+    public Product[] getBasket() {
         return listOfProducts;
     }
 
